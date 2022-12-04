1f19ebb1e0961081c538f9bd9a552683df7ec9df
Les Chinyanga <les.chinyanga@africacode.academy>
2022-12-04 17:10:54 +0200

    Test connection to database

--- src/main/java/com/github/chinyangatl/redfox/HelloServlet.java
+++ src/main/java/com/github/chinyangatl/redfox/HelloServlet.java
@@ -1,12 +1,20 @@
 package com.github.chinyangatl.redfox;
 
 import java.io.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
+import jakarta.annotation.Resource;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
+import javax.sql.DataSource;
+
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
+    @Resource(name="jdbc/redfox")
+    private DataSource dataSource;
     private String message;
 
     public void init() {
@@ -14,13 +22,24 @@ public class HelloServlet extends HttpServlet {
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+        PrintWriter printWriter = response.getWriter();
+        response.setContentType("text/plain");
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try{
+            connection = dataSource.getConnection();
+            String sql = "select * from admin";
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(sql);
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+            while (resultSet.next()) {
+                String email = resultSet.getString("email");
+                printWriter.println(email);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void destroy() {
