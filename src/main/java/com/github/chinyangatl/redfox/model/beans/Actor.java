7d6824646e53c2c858060168bd67a393e81c9c5a
Les Chinyanga <les.chinyanga@africacode.academy>
2022-12-05 02:16:18 +0200

    Refactor beans for convience

--- src/main/java/com/github/chinyangatl/redfox/model/beans/Actor.java
+++ src/main/java/com/github/chinyangatl/redfox/model/beans/Actor.java
@@ -1,6 +1,9 @@
 package com.github.chinyangatl.redfox.model.beans;
 
 public class Actor extends FilmActor{
+    public Actor(String firstName, String lastName, String dob) {
+        super(firstName, lastName, dob);
+    }
     public Actor(int id, String firstName, String lastName, String dob) {
         super(id, firstName, lastName, dob);
     }
